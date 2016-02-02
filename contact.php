69c06001fa74f70626dccfeb3790e38dd8970cbe
1311059 <a.e.obayemi@rgu.ac.uk>
2016-02-02 09:53:33 +0000

    Second commit version 2

---
+++ contact.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <title>Contact</title>
+</head>
+<header><h2>Azure Deployment Test | Contact</h2></header>
+<nav><a href="index.html">Home</a> &nbsp; <a href="contact.php">Contact</a> </nav>
+<div>
+    <
+    <label id="contactName">Name:
+    <input type="text" name="contactName" id="contactName" />
+    </label
+    <label id="contactMsg">Message
+        <textarea rows="20" cols="30" id="contactMsg" name="contactMsg"></textarea>
+    </label>
+
+</div>
+<p>Email: a.e.obayemi@rgu.ac.uk</p>
+<body>
+</body>
+</html>
